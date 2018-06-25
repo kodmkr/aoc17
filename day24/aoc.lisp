d3bbc712b83b1fdd5ca441b024ec3d7ad27c2e38
kodmkr <31863107+kodmkr@users.noreply.github.com>
2018-06-25 22:04:20 +0200

    Skeleton for day 24.

---
+++ day24/aoc.lisp
@@ -0,0 +1,52 @@
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload "cl-arrows")
+  (ql:quickload "cl-ppcre")
+  (ql:quickload "alexandria"))
+
+(defpackage :day24
+  (:use :cl :cl-arrows)
+  (:export :day-24-a
+           :day-24-b))
+
+(in-package :day24)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+;; --------------------------------------------------------------------------------
+
+(defun day-24-a ())
+(defun day-24-b ())
